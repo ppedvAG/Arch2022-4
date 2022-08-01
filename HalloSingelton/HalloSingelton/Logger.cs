a9f43e52da8716fc03e2afadc714d6d5894edd43
André <AndreR@ppedv.de>
2022-08-01 11:19:00 +0200

    Singelton Pattern

---
+++ HalloSingelton/HalloSingelton/Logger.cs
@@ -0,0 +1,31 @@
+﻿namespace HalloSingelton
+{
+    internal class Logger
+    {
+        private static Logger _logger;
+        private static object _sync = new object();
+
+        public static Logger Instance
+        {
+            get
+            {
+                lock (_sync)
+                {
+                    _logger ??= new Logger();
+                }
+
+                return _logger;
+            }
+        }
+
+        private Logger()
+        {
+            Log("Neuer Logger");
+        }
+
+        public void Log(string msg)
+        {
+            Console.WriteLine($"[{DateTime.Now:G}] {msg}");
+        }
+    }
+}
---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,8 @@
+﻿using HalloSingelton;
+
+Console.WriteLine("Hello Singelton");
+
+//var logger = new Logger();
+
+Parallel.For(1, 20, i => Logger.Instance.Log($"Test {i}"));
+
