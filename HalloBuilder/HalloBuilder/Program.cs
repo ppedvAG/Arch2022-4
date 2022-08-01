0915ed6d190f145322243f782562a73d8c4022a3
André <AndreR@ppedv.de>
2022-08-01 12:00:08 +0200

    Builder Pattern

---
+++ HalloBuilder/HalloBuilder/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HalloBuilder;
+
+Console.WriteLine("Hello, World!");
+
+
+Schrank schrank1 = new Schrank.Builder()
+                              .SetAnzahlTüren(5)
+                              .SetOberfläche(Oberfläche.Lackiert)
+                              .SetFarbe("Gelb")
+                              .Create();
+
+Schrank schrank2 = new Schrank.Builder()
+                              .SetAnzahlTüren(1)
+                              .SetOberfläche(Oberfläche.Unbehandelt)
+                              .SetFarbe("Pink")
+                              .Create();
---
+++ HalloBuilder/HalloBuilder/Schrank.cs
@@ -0,0 +1,62 @@
+﻿namespace HalloBuilder
+{
+    public class Schrank
+    {
+        public int AnzahlTüren { get; private set; }
+        public Oberfläche Oberfläche { get; private set; }
+        public string Farbe { get; private set; }
+        public int AnzahlBöden { get; private set; }
+        public bool Metallschienen { get; private set; }
+        public bool Kleiderstange { get; private set; }
+
+        private Schrank()
+        { }
+
+        public class Builder
+        {
+            Schrank newSchrank = new Schrank();
+
+            public Builder SetAnzahlTüren(int anzahl)
+            {
+                if (anzahl < 0 || anzahl > 7)
+                    throw new ArgumentException("Es sind nut 2-7 Türen Erlaubt");
+
+                newSchrank.AnzahlTüren = anzahl;
+
+                return this;
+            }
+
+            public Builder SetOberfläche(Oberfläche oberfläche)
+            {
+                newSchrank.Oberfläche = oberfläche;
+
+                if (oberfläche != Oberfläche.Lackiert)
+                    newSchrank.Farbe = string.Empty;
+
+                return this;
+            }
+
+            public Builder SetFarbe(string farbe)
+            {
+                if (newSchrank.Oberfläche != Oberfläche.Lackiert)
+                    throw new ArgumentException();
+
+                newSchrank.Farbe = farbe;
+
+                return this;
+            }
+
+            public Schrank Create()
+            {
+                return newSchrank;
+            }
+        }
+    }
+
+    public enum Oberfläche
+    {
+        Unbehandelt,
+        Gewachst,
+        Lackiert
+    }
+}
