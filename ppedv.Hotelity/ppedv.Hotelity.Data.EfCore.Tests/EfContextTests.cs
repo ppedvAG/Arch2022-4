8c8b730a0cd8915af0b8761960edd98b264abf63
André <AndreR@ppedv.de>
2022-08-02 12:42:13 +0200

    Tests mit AutoFixture und FluentAssertions

--- ppedv.Hotelity/ppedv.Hotelity.Data.EfCore.Tests/EfContextTests.cs
+++ ppedv.Hotelity/ppedv.Hotelity.Data.EfCore.Tests/EfContextTests.cs
@@ -1,3 +1,10 @@
+using AutoFixture;
+using AutoFixture.Kernel;
+using FluentAssertions;
+using ppedv.Hotelity.Model;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
 namespace ppedv.Hotelity.Data.EfCore.Tests
 {
     public class EfContextTests
@@ -7,10 +14,92 @@ namespace ppedv.Hotelity.Data.EfCore.Tests
         {
             var con = new EfContext();
             con.Database.EnsureDeleted();
-            
-            var result =  con.Database.EnsureCreated();
+
+            var result = con.Database.EnsureCreated();
 
             Assert.True(result);
+        }
+
+        [Fact]
+        public void Can_CRUD_Gast()
+        {
+            var gast = new Gast() { Name = $"Fred_{Guid.NewGuid()}" };
+            var newName = $"Wilma_{Guid.NewGuid()}";
+
+            using (var con = new EfContext())
+            {
+                con.Add(gast);
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext())
+            {
+                var loaded = con.Gaeste.Find(gast.Id);
+                Assert.NotNull(loaded);
+                Assert.Equal(gast.Name, loaded?.Name);
+
+                loaded.Name = newName;
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext())
+            {
+                var loaded = con.Gaeste.Find(gast.Id);
+                Assert.Equal(newName, loaded?.Name);
+
+                con.Remove(loaded);
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext())
+            {
+                var loaded = con.Gaeste.Find(gast.Id);
+                Assert.Null(loaded);
+            }
+        }
+
+
+        [Fact]
+        [Trait("Category", "Systemtest")]
+        public void Can_Create_Gast_AutoFix()
+        {
+            var fix = new Fixture();
+            fix.Behaviors.Add(new OmitOnRecursionBehavior());
+            fix.Customizations.Add(new PropertyNameOmitter(nameof(Entity.Id)));
+
+            var gast = fix.Create<Gast>();
+
+            using (var con = new EfContext())
+            {
+                con.Add(gast);
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext())
+            {
+                var loaded = con.Gaeste.Include(x => x.Buchung).ThenInclude(x => x.Zimmer).Where(x => x.Id == gast.Id).First();
+                loaded.Should().BeEquivalentTo(gast, x => x.IgnoringCyclicReferences());
+            }
+
+        }
+
+        internal class PropertyNameOmitter : ISpecimenBuilder
+        {
+            private readonly IEnumerable<string> names;
+
+            internal PropertyNameOmitter(params string[] names)
+            {
+                this.names = names;
+            }
+
+            public object Create(object request, ISpecimenContext context)
+            {
+                var propInfo = request as PropertyInfo;
+                if (propInfo != null && names.Contains(propInfo.Name))
+                    return new OmitSpecimen();
+
+                return new NoSpecimen();
+            }
         }
     }
 }
