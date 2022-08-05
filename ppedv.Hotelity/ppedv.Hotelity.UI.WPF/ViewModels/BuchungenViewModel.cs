87986ca4c3b0e64700a5ec9a1a6b90578c4fa913
André <AndreR@ppedv.de>
2022-08-05 10:29:38 +0200

    WPF UI mit Viewmodel und Binding

---
+++ ppedv.Hotelity/ppedv.Hotelity.UI.WPF/ViewModels/BuchungenViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ppedv.Hotelity.Logic;
+using ppedv.Hotelity.Model.DomainModel;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace ppedv.Hotelity.UI.WPF.ViewModels
+{
+    internal class BuchungenViewModel
+    {
+        //todo di
+        Core core = new Core(new Data.EfCore.EfMainRepository());
+
+        public ObservableCollection<Buchung> Buchungen { get; set; }
+
+        public Buchung SelecteBuchung { get; set; }
+
+        public BuchungenViewModel()
+        {
+            Buchungen = new ObservableCollection<Buchung>(core.UnitOfWork.BuchungenRepository.Query().ToList());
+        }
+    }
+}
