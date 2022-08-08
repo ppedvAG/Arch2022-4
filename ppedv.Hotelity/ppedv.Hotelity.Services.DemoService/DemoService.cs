29dbdc68c4e222ccbb2432148c7a31a6e3b33af9
André <AndreR@ppedv.de>
2022-08-08 09:52:35 +0200

    Demo Service

---
+++ ppedv.Hotelity/ppedv.Hotelity.Services.DemoService/DemoService.cs
@@ -0,0 +1,36 @@
+﻿using Bogus;
+using Microsoft.VisualBasic;
+using ppedv.Hotelity.Model.Contracts.Services;
+using ppedv.Hotelity.Model.DomainModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ppedv.Hotelity.Services.DemoService
+{
+    public class DemoService : IDemoService
+    {
+        public IEnumerable<Gast> GetDemoGaeste()
+        {
+            var gastFaker = new Faker<Gast>("de")
+                            .UseSeed(5)
+                            .RuleFor(x => x.Name, x => x.Name.FullName())
+                            .RuleFor(x => x.GebDatum, x => x.Date.Past(30));
+
+            var buchungFaker = new Faker<Buchung>()
+                                .UseSeed(5)
+                                .RuleFor(x => x.Buchungsdatum, x => x.Date.Recent(14))
+                                .RuleFor(x => x.Von, x => x.Date.Soon(5))
+                                .RuleFor(x => x.Bis, x => x.Date.Soon(10));
+
+            gastFaker.RuleFor(x => x.Buchung, x => buchungFaker.Generate(x.Random.Int(1, 3)));
+
+            foreach (var g in gastFaker.Generate(20))
+            {
+                foreach (var b in g.Buchung)
+                {
+                    b.Gast = g;
+                }
+                yield return g;
+            }
+        }
+    }
+}
--- ppedv.Hotelity/ppedv.Hotelity.UI.WPF/ViewModels/BuchungenViewModel.cs
+++ ppedv.Hotelity/ppedv.Hotelity.UI.WPF/ViewModels/BuchungenViewModel.cs
@@ -2,7 +2,9 @@
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.VisualBasic;
 using ppedv.Hotelity.Logic;
+using ppedv.Hotelity.Model.Contracts.Services;
 using ppedv.Hotelity.Model.DomainModel;
+using ppedv.Hotelity.Services.DemoService;
 using ppedv.Hotelity.UI.WPF.Commands;
 using System;
 using System.Collections.ObjectModel;
@@ -14,10 +16,9 @@ namespace ppedv.Hotelity.UI.WPF.ViewModels
 {
     internal class BuchungenViewModel : ObservableObject
     {
-        //todo di
+        //todo di !!!!
         Core core = new Core(new Data.EfCore.EfMainRepository());
-
-        //public event PropertyChangedEventHandler? PropertyChanged;
+        IDemoService demoService = new DemoService();
 
         public ObservableCollection<Buchung> Buchungen { get; set; }
 
@@ -28,10 +29,12 @@ namespace ppedv.Hotelity.UI.WPF.ViewModels
             set
             {
                 SetProperty(ref selecteBuchung, value);
+
                 //selecteBuchung = value;
                 //OnPropertyChanging();
                 //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SelecteBuchung"));
                 OnPropertyChanged(nameof(BuchungsAlter));
+                OnPropertyChanged(nameof(HauptGastName));
                 //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(BuchungsAlter)));
 
             }
@@ -40,6 +43,8 @@ namespace ppedv.Hotelity.UI.WPF.ViewModels
         public ICommand SaveCommand { get; set; }
 
         public ICommand SaveCommand2 { get; set; }
+
+        public ICommand DemoCommand { get; set; }
 
         public string BuchungsAlter
         {
@@ -57,6 +62,26 @@ namespace ppedv.Hotelity.UI.WPF.ViewModels
 
             SaveCommand = new SaveCommand(core.UnitOfWork);
             SaveCommand2 = new RelayCommand(() => core.UnitOfWork.SaveAll());
+
+            DemoCommand = new RelayCommand(() => UserWantsToCreateDemoData());
+        }
+
+        private void UserWantsToCreateDemoData()
+        {
+            demoService.GetDemoGaeste().ToList()
+                .ForEach(x => x.Buchung.ToList()
+                    .ForEach(y => Buchungen.Add(y)));
+        }
+
+        public string HauptGastName
+        {
+            get
+            {
+                if (SelecteBuchung == null || SelecteBuchung.Gast == null)
+                    return "---";
+
+                return SelecteBuchung.Gast.Name;
+            }
         }
     }
 }
